97ba031c34f1dadf65581b9727b4e05d6f9fa106
kailany <kailanyalves04@gmail.com>
2025-08-21 16:48:37 -0300

    refatora modal de logout para tornar responsivo

--- views/includes/modal.inc.php
+++ views/includes/modal.inc.php
@@ -4,21 +4,29 @@
 
 <!-- Modal de logout -->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="logoutModalLabel">Sair do sistema</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body text-center">
-            Você realmente deseja sair do sistema?
-        </div>
-        <div class="modal-footer">
-            <a class="btn btn-primary" href="../controllers/controllerUsuario.php?opcao=7">Sair</a>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
-        </div>
-        </div>
+      
+      <div class="modal-header">
+        <h5 class="modal-title" id="logoutModalLabel">Sair do sistema</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      
+      <div class="modal-body text-center">
+        Você realmente deseja sair do sistema?
+      </div>
+      
+      <div class="modal-footer d-flex flex-column flex-sm-row justify-content-center">
+        <a class="btn btn-primary mb-2 mb-sm-0 mr-sm-2 w-100 w-sm-auto" href="../controllers/controllerUsuario.php?opcao=7">
+          Sair
+        </a>
+        <button type="button" class="btn btn-secondary w-100 w-sm-auto" data-dismiss="modal">
+          Não
+        </button>
+      </div>
+      
     </div>
+  </div>
 </div>
